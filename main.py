aa2b9ce4f3a757f6e0a0e3e2b64cc3f7e7d38417
AntonFritsche <antonfritschedresden@gmail.com>
2026-02-03 23:35:55 +0100

    added collection logic for customer/moderator data

--- main.py
+++ main.py
@@ -1,39 +1,46 @@
 import pyautogui
 import torch
 import asyncio
+import easyocr
+import random
+import time
 
+from mss import mss
+from PIL import Image
 from datetime import datetime
 from transformers import pipeline
 
 
 class ChatBot:
-    def __init__(self, name: str ,time: int, llm: str, **kwargs):
+    def __init__(self, name: str, time: int, llm: str, **kwargs):
         super.__init__(**kwargs)
-        self.name = name
-        self.start_time = datetime.now()
-        self.time = time
-        self.llm = llm
+        self.name: str = name
+        self.start_time: datetime = datetime.now()
+        self.time: int = time
+        self.llm: str = llm
         self.pipe = None
 
-        self.number_messages = 0
-        self.context = []
-        self.customer_data = {
+        self.number_messages: int = 0
+        self.context: list = []
+        self.customer_data: dict = {
             "Name": "",
             "Age": "",
             "City": "",
             "Job": "",
             "Sex Preferences": "",
             "Booked Holidays": "",
-            "Others": ""
+            "Others": "",
+            "Custom Data": []
         }
-        self.moderator_data = {
+        self.moderator_data: dict = {
             "Name": "",
             "Age": "",
             "City": "",
             "Job": "",
             "Sex Preferences": "",
             "Booked Holidays": "",
-            "Others": ""
+            "Others": "",
+            "Custom Data": []
         }
 
     def write_message(self, msg: str):
@@ -89,11 +96,62 @@ class ChatBot:
 
     def collect_customer_data(self):
         """collects the customer's data"""
-        pass
+        with mss() as sct:
+            monitor_customer = {"top": 0, "left": 0, "width": 0, "height": 0}
+            monitur_customer_custom_data = {"top": 0, "left": 0, "width": 0, "height": 0}
+
+            customer_image = sct.grab(monitor_customer)
+            customer_custom_data_image = sct.grab(monitur_customer_custom_data)
+
+            customer_image = Image.frombytes("RGB", customer_image.size, customer_image.bgra, "raw", "BGRX")
+            customer_custom_data_image = Image.frombytes("RGB", customer_custom_data_image.size, customer_custom_data_image.bgra, "raw", "BGRX")
+
+            customer_image.save("screenshots/customer_data.png")
+            customer_custom_data_image.save("screenshots/customer_custom_data.png")
+
+        reader = easyocr.Reader(['de', 'en'], gpu=False)
+        customer_text = reader.readtext('screenshots/customer_data.png', detail=0)
+        customer_custom_text = reader.readtext('screenshots/costumer_custom_data.png', detail=0)
+
+        self.customer_data["Name"] = customer_text[0].split("Name:")[1]
+        self.customer_data["Age"] = customer_text[1].split("Age:")[1]
+        self.customer_data["City"] = customer_text[2].split("City:")[1]
+        self.customer_data["Job"] = customer_text[3].split("Job:")[1]
+        self.customer_data["Sex Preferences"] = customer_text[4].split("Sex Preferences:")[1]
+        self.customer_data["Booked Holidays"] = customer_text[5].split("Booked Holidays:")[1]
+        self.customer_data["Others"] = customer_text[6].split("Others:")[1]
+
+        self.customer_data["Custom Data"] = " ".join(customer_custom_text)
 
     def collect_moderator_data(self):
         """collects the moderator's data"""
-        pass
+        with mss() as sct:
+            monitor_moderator = {"top": 0, "left": 0, "width": 0, "height": 0}
+            monitor_moderator_custom_data = {"top": 0, "left": 0, "width": 0, "height": 0}
+
+            moderator_image = sct.grab(monitor_moderator)
+            moderator_custom_data_image = sct.grab(monitor_moderator_custom_data)
+
+            moderator_image = Image.frombytes("RGB", moderator_image.size, moderator_image.bgra, "raw", "BGRX")
+            moderator_custom_data_image = Image.frombytes("RGB", moderator_custom_data_image.size, moderator_custom_data_image.bgra, "raw", "BGRX")
+
+            moderator_image.save("screenshots/moderator_data.png")
+            moderator_custom_data_image.save("screenshots/moderator_custom_data.png")
+
+
+        reader = easyocr.Reader(['de', 'en'], gpu=False)
+        moderator_text = reader.readtext('screenshots/moderator_data.png', detail=0)
+        moderator_custom_text = reader.readtext('screenshots/moderator_custom_data.png', detail=0)
+
+        self.moderator_data["Name"] = moderator_text[0].split("Name:")[1]
+        self.moderator_data["Age"] = moderator_text[1].split("Age:")[1]
+        self.moderator_data["City"] = moderator_text[2].split("City:")[1]
+        self.moderator_data["Job"] = moderator_text[3].split("Job:")[1]
+        self.moderator_data["Sex Preferences"] = moderator_text[4].split("Sex Preferences:")[1]
+        self.moderator_data["Booked Holidays"] = moderator_text[5].split("Booked Holidays:")[1]
+        self.moderator_data["Others"] = moderator_text[6].split("Others:")[1]
+
+        self.moderator_data["Custom Data"] = " ".join(moderator_custom_text)
 
     def load_llm(self):
         """loads the weights of the llm"""
@@ -119,9 +177,9 @@ class ChatBot:
         """checks if new tasks are available"""
         while True:
             # task logik
-            has_task = False
-            if has_task:
+            if pyautogui.locateOnScreen("waiting_for_session.png") is not None:
                 return True
+
             await asyncio.sleep(5)
 
     async def run(self):
@@ -130,10 +188,14 @@ class ChatBot:
         time = datetime.now()
         delta = time - self.start_time
         while delta.total_seconds() < self.time:
-            task = await self.task_queue.get()
+            task = await self.checks_new_task.get()
 
             if task:
                 # collects data
+                random.seed()
+                random_wait_time = random.random() * 5
+                time.sleep(random_wait_time)
+
                 self.collect_messages()
                 self.collect_customer_data()
                 self.collect_moderator_data()
